94fe2052586ee5b18655386bdc072e95e3e4f2f2
Zach Himsel <zach@himsel.net>
2017-02-09 19:54:43 -0700

    Set up logging

--- r53-dns-updater.py
+++ r53-dns-updater.py
@@ -19,6 +19,12 @@ To protect incorrect configurations or accidental typos, this script will not
 overwrite any records that do not match the expected A-record type.
 """
 from docopt import docopt
+import logging
+import logging.handlers
+
+# Set up global logging object
+log = logging.getLogger(__name__)
+log.propagate = False
 
 
 def main():
@@ -26,6 +32,25 @@ def main():
 
     # Init docopt arguments
     args = docopt(__doc__, help=True)
+
+    # Determine loglevel
+    if args['--verbose']:
+        log_level = 'INFO'
+    else:  # default to ERROR if no override is given
+        log_level = 'ERROR'
+    log_level_num = getattr(logging, log_level.upper(), None)
+    if not isinstance(log_level_num, int):
+        raise ValueError('Invalid log level: {}'.format(log_level))
+    log.setLevel(log_level_num)
+    logging.basicConfig(level=log_level_num)
+
+    # Set up console logger
+    log_console = logging.StreamHandler()
+    log_console.setLevel(logging.DEBUG)
+    log_console.setFormatter(logging.Formatter(
+        fmt='%(levelname)s:%(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S %Z'))
+    log.addHandler(log_console)
 
 
 if __name__ == "__main__":
